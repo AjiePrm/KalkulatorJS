29b5f36d77320aa7729e67970e8ff17a637eeab8
ajieprima <ajiee.salahudin@gmail.com>
2023-04-04 15:08:16 +0700

    add logic for keyboard interaction

--- app.js
+++ app.js
@@ -8,13 +8,50 @@ keys.forEach (key=>{
     key.addEventListener("click", calculate);
 });
 
+document.onkeydown = function(e) {
+    let key_pressed = e.key;
+    let regex = /^[0-9-+*=/.()]+$/;
+    //let eValue = "0"
+    //console.log(key_pressed)
+
+    if (key_pressed === "Escape") {
+        output.innerText = "";
+        result.innerText = "0";
+        return;    
+    }
+
+    if (key_pressed === "Backspace") {
+        output.textContent = output.textContent.substr(0,output.textContent.length-1);
+        return;
+    }
+
+    if (key_pressed === "=" || key_pressed ==="Enter" ) {
+            result.innerText = eval(output.innerText);
+            //eValue = result.innerText
+            //output.innerText = ""
+            //console.log(`evalue = ${eValue}`);
+            return;
+    }
+    
+    else if (key_pressed.match(regex)){
+        output.textContent += key_pressed;
+        return;
+    }
+    else {
+        if(result.innerText === "0"){
+            result.innerText = "0"
+        }
+            result.innerText = eval(output.innerText);
+            //console.log(`evalueini = ${result.innerText}`);
+            //alert('Ini kalkulator bang bukan Chat GPT')
+    }
+}
+
 function calculate() {
     let buttonText = this.innerText;
     if (buttonText === "AC") {
         output.innerText = "";
         result.innerText = "0";
-        result.style.animation = "";
-        output.style.animation = "";
         return;    
     }
 
@@ -25,10 +62,7 @@ function calculate() {
 
     if (buttonText === "=") {
         result.innerText = eval(output.innerText);
-        result.style.animation = "big 0.5s ease-in-out";
-        output.style.animation = "small 0.5s ease-in-out";
-        result.style.animationFillMode = "forwards";
-        output.style.animationFillMode = "forwards";
+        
     }
 
     else {
